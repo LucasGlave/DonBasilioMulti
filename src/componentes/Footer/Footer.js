132fa12cfd0160272a25576c7bbc5aad0d03d8ce
LucasGlave <lucasglave7@gmail.com>
2024-04-15 15:19:52 -0300

    work in modal calendly

--- src/componentes/Footer/Footer.js
+++ src/componentes/Footer/Footer.js
@@ -1,16 +1,18 @@
 import "./Footer.scss";
 import { FaFacebook, FaInstagram, FaPhone } from "react-icons/fa";
-
 import FooterResponsive from "./FooterResponsive";
 
 const Footer = () => {
   return (
     <div className="footer-back">
+      <div className="designed">
         <div className="footer-desktop">
           <div className="footIzq">
             <div className="nav-footer">
               <a href="">
-                <h6><FaFacebook /></h6>
+                <h6>
+                  <FaFacebook />
+                </h6>
               </a>
               <a href="">
                 <h6>
@@ -22,17 +24,17 @@ const Footer = () => {
                   <h6>
                     <ion-icon name="mail-outline"></ion-icon>
                   </h6>
-                  <h6>
-                    donbasilio@gmail.com
-                  </h6>
+                  <h6>donbasilio@gmail.com</h6>
                 </div>
               </a>
               <a href="">
                 <div className="phone-container">
                   <div className="svgPhone">
-                      <h6><FaPhone /></h6>
+                    <h6>
+                      <FaPhone />
+                    </h6>
                   </div>
-                    <h6> +54 2923 000000</h6>
+                  <h6> +54 2923 000000</h6>
                 </div>
               </a>
             </div>
@@ -55,7 +57,11 @@ const Footer = () => {
             </div>
           </div>
         </div>
-        <FooterResponsive />
+        <p className="designedBy">
+          designed by <span>SeptemDev Studios</span>
+        </p>
+      </div>
+      <FooterResponsive />
     </div>
   );
 };
