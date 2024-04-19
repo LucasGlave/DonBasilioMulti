bf41e376cc9300984a7b97c9e4cc03888b76c30d
LucasGlave <111156600+LucasGlave@users.noreply.github.com>
2024-04-19 18:02:10 -0300

    Update dataPeluqueros.js

--- src/componentes/SacarTurno/dataPeluqueros.js
+++ src/componentes/SacarTurno/dataPeluqueros.js
@@ -4,16 +4,16 @@ export const peluqueros = [
     {
       "nombre": "Juan Perez",
       "foto": "peluquero1.jpeg",
-      "url": "https://calendly.com/septemdevstudios/sede-1-peluquero-1"
+      "url": "https://calendly.com/septemdevstudio/sede-1-peluquero-1"
     },
     {
       "nombre": "Leandro García",
       "foto": "peluquero2.jpeg",
-      "url": "https://calendly.com/septemdevstudios/sede-1-peluquero-2"
+      "url": "https://calendly.com/septemdevstudio/sede-1-peluquero-2"
     },
     {
       "nombre": "Pedro Sánchez",
       "foto": "peluquero3.jpeg",
-      "url": "https://calendly.com/septemdevstudios/sede-1-peluquero-2"
+      "url": "https://calendly.com/septemdevstudio/sede-1-peluquero-2"
     }
 ]
